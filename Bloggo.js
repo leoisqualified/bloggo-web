a473ad5fd6f6991d9c026c558e2edaaeabb78abf
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2023-02-26 19:55:02 +0000

    Add files via upload

---
+++ Bloggo.js
@@ -0,0 +1,14 @@
+var myName='Andy';
+var myAge= 17;
+if (myAge>=18 && myName=='Andy') {
+    
+    document.write('Welcome to bloggo!')
+}
+else if(myName!=='Andy') {
+    
+    document.write('Wrong Username')
+}
+else{
+    
+    document.write('You are under 18')
+}
